d204c32bc2a08efcf05337fe1ed6cb36b62385e9
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-06-02 16:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const volume = document.querySelector('input[name="volume"]');
+const playbackSpeed = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', () => {
+    toggle.textContent = '❚ ❚';
+});
+
+video.addEventListener('pause', () => {
+    toggle.textContent = '►';
+});
+
+volume.addEventListener('input', () => {
+    video.volume = volume.value;
+});
+
+playbackSpeed.addEventListener('input', () => {
+    video.playbackRate = playbackSpeed.value;
+});
+
+skipButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        video.currentTime += Number(button.dataset.skip);
+    });
+});
+
+function updateProgress() {
+    const percent =
+        (video.currentTime / video.duration) * 100;
+
+    progressFilled.style.width = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', updateProgress);
+
+progress.addEventListener('click', (e) => {
+    const seekTime =
+        (e.offsetX / progress.offsetWidth) *
+        video.duration;
+
+    video.currentTime = seekTime;
+});
